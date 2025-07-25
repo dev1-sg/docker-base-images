4957cde250ffdec61df69974473cb42f43f7337f
antyung47 <antyung47@gmail.com>
2025-07-26 04:30:33 +0800

    add uv

--- tests/uv_test.go
+++ tests/uv_test.go
@@ -9,7 +9,7 @@ import (
 	"github.com/testcontainers/testcontainers-go"
 )
 
-var PythonUv = struct {
+var Uv = struct {
 	AWS_DEFAULT_REGION              string
 	AWS_ECR_PUBLIC_URI              string
 	AWS_ECR_PUBLIC_REPOSITORY_GROUP string
@@ -19,16 +19,16 @@ var PythonUv = struct {
 	AWS_DEFAULT_REGION:              "us-east-1",
 	AWS_ECR_PUBLIC_URI:              "public.ecr.aws/dev1-sg",
 	AWS_ECR_PUBLIC_REPOSITORY_GROUP: "ubuntu",
-	AWS_ECR_PUBLIC_IMAGE_NAME:       "python-uv",
+	AWS_ECR_PUBLIC_IMAGE_NAME:       "uv",
 	AWS_ECR_PUBLIC_IMAGE_TAG:        "latest",
 }
 
-func TestContainersGoPullPythonUv(t *testing.T) {
+func TestContainersGoPullUv(t *testing.T) {
 	ctx := context.Background()
 	for attempt := 0; attempt < 3; attempt++ {
 		container, e := testcontainers.GenericContainer(ctx, testcontainers.GenericContainerRequest{
 			ContainerRequest: testcontainers.ContainerRequest{
-				Image: PythonUv.AWS_ECR_PUBLIC_URI + "/" + PythonUv.AWS_ECR_PUBLIC_REPOSITORY_GROUP + "/" + PythonUv.AWS_ECR_PUBLIC_IMAGE_NAME + ":" + PythonUv.AWS_ECR_PUBLIC_IMAGE_TAG,
+				Image: Uv.AWS_ECR_PUBLIC_URI + "/" + Uv.AWS_ECR_PUBLIC_REPOSITORY_GROUP + "/" + Uv.AWS_ECR_PUBLIC_IMAGE_NAME + ":" + Uv.AWS_ECR_PUBLIC_IMAGE_TAG,
 			},
 		})
 		require.NoError(t, e)
@@ -36,11 +36,11 @@ func TestContainersGoPullPythonUv(t *testing.T) {
 	}
 }
 
-func TestContainersGoExecPythonUv(t *testing.T) {
+func TestContainersGoExecUv(t *testing.T) {
 	ctx := context.Background()
 	container, err := testcontainers.GenericContainer(ctx, testcontainers.GenericContainerRequest{
 		ContainerRequest: testcontainers.ContainerRequest{
-			Image: PythonUv.AWS_ECR_PUBLIC_URI + "/" + PythonUv.AWS_ECR_PUBLIC_REPOSITORY_GROUP + "/" + PythonUv.AWS_ECR_PUBLIC_IMAGE_NAME + ":" + PythonUv.AWS_ECR_PUBLIC_IMAGE_TAG,
+			Image: Uv.AWS_ECR_PUBLIC_URI + "/" + Uv.AWS_ECR_PUBLIC_REPOSITORY_GROUP + "/" + Uv.AWS_ECR_PUBLIC_IMAGE_NAME + ":" + Uv.AWS_ECR_PUBLIC_IMAGE_TAG,
 			Cmd:   []string{"sleep", "10"},
 		},
 		Started: true,
