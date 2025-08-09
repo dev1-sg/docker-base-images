461420193de49215dfea90476cdc79b530373b08
antyung47 <antyung47@gmail.com>
2025-08-09 15:42:58 +0800

    optimize image size with locales over locales-all

--- src/java/tags.sh
+++ src/java/tags.sh
@@ -1,10 +1,10 @@
 #!/usr/bin/env bash
 
-ubuntu=($(sed -n 's/^FROM .*:\([^ -]*\).*/\1/p' Dockerfile | head -2))
-java=($(sed -n 's/^FROM .*:\([^ -]*\).*/\1/p' Dockerfile | head -1))
+ubuntu=($(sed -n 's/^FROM .*:\([^ -]*\).*/\1/p' Dockerfile | head -1))
+java=($(sed -n 's/^ARG JAVA_VERSION=\(.*\)/\1/p' Dockerfile | head -1))
 
 if [[ -n "${java}" && -n "${ubuntu}" ]]; then
-    echo "${java}-${ubuntu[1]}"
+    echo "${java}-${ubuntu}"
 else
     echo "latest"
 fi
